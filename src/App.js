421535d4b51f2e92c63c60f41cd8e7c5adedf0e4
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-28 12:15:29 -0400

    update header add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 // import Footer from './components/Footer';
-import ContactForm from './components/Contact';
-import About from './components/About';
+// import ContactForm from './components/Contact';
+// import About from './components/About';
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
       </Header>
       <main>
         <div>
-          <About></About>
-          <ContactForm></ContactForm>
+          {/* <About></About> */}
+          {/* <ContactForm></ContactForm> */}
         </div>
       </main>
     </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 // import { capitalizeFirstLetter } from "../../utils/helpers";
+import Nav from 'react-bootstrap/Nav';
 
-function Header() {
-    // const {
-    //     categories = [],
-    //     setCurrentCategory,
-    //     currentCategory,
-    //     contactSelected,
-    //     setContactSelected,
-    // } = props;
+function Header(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+        contactSelected,
+        setContactSelected,
+    } = props;
 
     return (
         <header className="flex-row px-1">
         <h2>
-            <a href="/">
+            <a className="mainPage" href="/">
             <span>
                 {" "}
                 📸
@@ -21,25 +22,26 @@ function Header() {
             Jeremy Jakobsons
             </a>
         </h2>
-        <nav>
-            <ul className="flex-row">
-            <li className="mx-2">
-                <a
-                href="#about"
-                // onClick={() => setContactSelected(false)}
-                >
-                About me
-                </a>
-            </li>
-            <li className='mx-2'>
-                <span>Contact</span>
-            </li>
-                <li>
-                <span>
-                </span>
-                </li>
-            </ul>
-        </nav>
+
+        <Nav className="justify-content-end" activeKey="/home">
+            <Nav.Item>
+                <Nav.Link href="/home">About Me</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Contact</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-3">Resume</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                Disabled
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
         </header>
     );
 }
