c17fd61f71af5e7ec7e4e6fe0ed987ff2c7bffbc
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-28 15:15:00 -0400

    update styling of header/nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,36 +12,37 @@ function Header(props) {
     } = props;
 
     return (
-        <header className="flex-row px-1">
-        <h2>
-            <a className="mainPage" href="/">
-            <span>
-                {" "}
-                📸
-            </span>
-            Jeremy Jakobsons
-            </a>
-        </h2>
+        <header className="nav-row">
+            <h2>
+                <a className="mainPage" href="/">
+                    <img className="iconPic" src={require('../../assets/images/OST_Kickoff_Headshots-262.jpg')}
+                    alt="Author Headshot"
+                    />
+                    <div className="pageTitle">
+                        Jeremy Jakobsons
+                    </div>
+                </a>
+            </h2>
 
-        <Nav className="justify-content-end" activeKey="/home">
-            <Nav.Item>
-                <Nav.Link href="/home">About Me</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Portfolio</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Contact</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-3">Resume</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                Disabled
-                </Nav.Link>
-            </Nav.Item>
-        </Nav>
+            <Nav className="justify-content-end" activeKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">About Me</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Portfolio</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Contact</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-3">Resume</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
         </header>
     );
 }
