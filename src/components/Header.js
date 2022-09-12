b957a67f019225bfb1b32b9ddf3db7b25324ae29
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-09-11 22:24:05 -0400

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,44 +2,6 @@ import React, { Component } from "react";
 import ParticlesBg from "particles-bg";
 import Fade from "react-reveal";
 
-// function Header(props) {
-//     const {
-//         categories = [],
-//         setCurrentCategory,
-//         currentCategory,
-//         contactSelected,
-//         setContactSelected,
-//     } = props;
-
-//     return (
-//         <header className="flex-row px-1">
-//             <h2>
-//                 <a className="mainPage" href="/">
-//                     {/* <img className="iconPic" src={require('../../assets/images/avatar.png')}
-//                     alt="Author Avatar Memoji"
-//                     />  */}
-//                     Jeremy Jakobsons
-//                 </a>
-//             </h2>
-
-//             <Nav className="flex-row justify-content-end" activeKey="/home">
-//                 <Nav.Item className="mx-2">
-//                     <Nav.Link href="/home">About Me</Nav.Link>
-//                 </Nav.Item>
-//                 <Nav.Item className="mx-2">
-//                     <Nav.Link eventKey="link-1">Portfolio</Nav.Link>
-//                 </Nav.Item>
-//                 <Nav.Item className="mx-2">
-//                     <Nav.Link eventKey="link-2">Contact</Nav.Link>
-//                 </Nav.Item>
-//                 <Nav.Item className="mx-2">
-//                     <Nav.Link eventKey="link-3">Resume</Nav.Link>
-//                 </Nav.Item>
-//             </Nav>
-//         </header>
-//     );
-// }
-
 class Header extends Component {
     render() {
         if (!this.props.data) return null;
